dec7595db85fc7b36883db7f292b9ed14136f6b0
Adam Tait <adamtaitcs@gmail.com>
2024-06-08 19:22:43 +0100

    [Navbar] Create base navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <header className="header">
+            <a href="/" className="logo">Adam Tait</a>
+
+            <nav className="navbar">
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Contact</a>
+            </nav>
+        </header>
+    )
+}
